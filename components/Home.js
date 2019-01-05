d7387928b03111f24e4e018409743e15374e9b52
Macbook <macbook@Macbooks-MacBook-Air.local>
2019-01-05 11:30:28 +0530

    working on carousel

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import { StyleSheet, Button, Text, View } from 'react-native'
-import MyCarousel from './MyCarousel';
+import MyCarousel from '../components/MyCarousel'
 
 class Home extends React.Component {
+  state = {
+    business: [
+      {
+        id: 'WpIAc9by5iU',
+        thumbnail: require('../assets/img/location_1.jpg'),
+        title: 'ECommerce'
+      },
+      {
+        id: 'sNPnbI1arSE',
+        thumbnail: require('../assets/img/location_2.jpg'),
+        title: 'FinTech'
+      },
+      {
+        id: 'VOgFZfRVaww',
+        thumbnail: require('../assets/img/location_3.jpg'),
+        title: 'Food'
+      },
+      {
+        id: 'VOgFZfRVqww',
+        thumbnail: require('../assets/img/location_4.jpg'),
+        title: 'Health'
+      }
+    ]
+  }
   render() {
     return (
       <View style={styles.container}>
-        <MyCarousel />
+        <MyCarousel
+          title={'Featured Business Categories'}
+          data={this.state.business}
+        />
       </View>
     )
   }
