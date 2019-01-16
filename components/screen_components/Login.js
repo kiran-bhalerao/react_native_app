3eea1d1bfe2fa7aab1eea735ca192f5e9bcc2569
kiranbhalerao123 <bhaleraok65@gmail.com>
2019-01-17 01:50:06 +0530

    working on login screen

--- components/screen_components/Login.js
+++ components/screen_components/Login.js
@@ -1,26 +1,118 @@
-import React from "react";
-import { StyleSheet, Text, View ,Button} from "react-native";
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ImageBackground,
+  TextInput,
+  TouchableWithoutFeedback,
+  ScrollView
+} from 'react-native'
 import CustomTab from '../custom_components/CustomTab'
 import Image from 'react-native-remote-svg'
+import THEME from '../APP_Theme'
 
 class Login extends React.Component {
+  state = {
+    username: '',
+    password: ''
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is Login Component!</Text>
-        <Button onPress={() => this.props.navigation.navigate('main')}  title='Go Home'/>
+        <Text
+          style={{
+            marginTop: 20,
+            fontSize: 20,
+            fontWeight: '800',
+            color: '#000'
+          }}
+        >
+          SICCA APP
+        </Text>
+        <View style={styles.topSection}>
+{/*          <ScrollView
+            style={{
+              width: '100%'
+            }}
+            contentContainerStyle={{ justifyContent: 'center', marginTop: 20 }}
+            showsVerticalScrollIndicator={false}
+          >*/}
+            <View style={styles.topSectionContainer}>
+              <View
+                style={{
+                  width: 140,
+                  height: 140,
+                  borderRadius: 20,
+                  backgroundColor: '#EBEBEB',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginBottom: 20
+                }}
+              >
+                <Text>Logo</Text>
+              </View>
+              <TextInput
+                style={styles.inputText}
+                autoFocus={true}
+                autoCorrect={false}
+                autoCapitalize={false}
+                placeholder="username"
+                placeholderTextColor="#A4A4A4"
+                underlineColorAndroid="transparent"
+                onChangeText={username => this.setState({ username })}
+                value={this.state.username}
+              />
+              <TextInput
+                style={styles.inputText}
+                secureTextEntry={true}
+                autoCorrect={false}
+                autoCapitalize={false}
+                placeholder="password"
+                placeholderTextColor="#A4A4A4"
+                underlineColorAndroid="transparent"
+                onChangeText={password => this.setState({ password })}
+                value={this.state.password}
+              />
+              <TouchableWithoutFeedback
+                onPress={() => this.props.navigation.navigate('main')}
+              >
+                <View style={styles.loginButton}>
+                  <Text style={{ color: '#fff' }}>LOGIN</Text>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          {/*</ScrollView>*/}
+        </View>
         <View style={styles.bottomSection}>
-        <Image
-          source={require('../../assets/svg/wave.svg')}
-          // style={{ marginTop: 1 }}
-        />
+          <ImageBackground
+            source={require('../../assets/img/wave.png')}
+            style={{
+              width: '100%',
+              height: 110,
+              justifyContent: 'flex-end',
+              paddingBottom: 10,
+              alignItems: 'center'
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 14,
+                color: '#EBEBEB',
+                fontFamily: 'Nunito-Regular'
+              }}
+            >
+              SICCA APP @2019
+            </Text>
+          </ImageBackground>
         </View>
       </View>
-    );
+    )
   }
 }
 
-export default Login;
+export default Login
 
 const styles = StyleSheet.create({
   container: {
@@ -29,10 +121,43 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between'
   },
+  topSection: {
+    // backgroundColor: 'yellow',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%'
+  },
   bottomSection: {
     // flexDirection: 'column',
     width: '100%'
     // backgroundColor: 'yellow'
     // justifyContent: 'space-between'
   },
-});
+  topSectionContainer: {
+    // backgroundColor: 'yellow',
+    alignItems: 'center',
+    width: '100%'
+  },
+  loginButton: {
+    backgroundColor: THEME.COLOR,
+    width: '50%',
+    paddingTop: 16,
+    paddingBottom: 16,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 24,
+    zIndex: 999
+  },
+  inputText: {
+    backgroundColor: '#EBEBEB',
+    width: '80%',
+    marginTop: 14,
+    paddingHorizontal: 20,
+    paddingVertical: 14,
+    borderRadius: 30,
+    justifyContent: 'center',
+    zIndex: 999
+  }
+})
